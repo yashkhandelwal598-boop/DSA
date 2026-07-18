b55b1440adbf6eecd98f20561aae4541a8f7f934
Yash Khandelwal <yashkhandelwal598@gmail.com>
2026-07-19 00:29:18 +0530

    correct brute force to LC-1539

--- BINARY_SEARCH/LC_1539_FIND_Kth_+ve.cpp
+++ BINARY_SEARCH/LC_1539_FIND_Kth_+ve.cpp
@@ -3,22 +3,14 @@ using namespace std;
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-         int num = 1;
-        vector<int> ans;
-        for(int i=0; i<arr.size(); i++){
-            if(num == arr[i]){
-                num++;
+        for(int i = 0; i<arr.size(); i++){
+            if(arr[i]<=k){
+                k++;
             }
             else{
-                if(num < arr[i]){
-                    ans.push_back(num);
-                    num++;
-                    }
-                }
+                break;
             }
-        for(int i=1; i<=k; i++){
-            ans.push_back(arr[arr.size()-1]+i);
         }
-        return ans[k-1];
+        return k;
     }
 };
